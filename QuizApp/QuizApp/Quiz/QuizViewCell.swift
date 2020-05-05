78ebb86635234eacfb43df4ef1971e6767b2a21e
Nina <nina.kolarec7@gmail.com>
2020-05-05 12:03:14 +0200

    Added layout for list of quizes

---
+++ QuizApp/QuizApp/Quiz/QuizViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  QuizViewCell.swift
+//  QuizApp
+//
+//  Created by five on 05/05/2020.
+//  Copyright © 2020 Nina Kolarec. All rights reserved.
+//
+
+import UIKit
+
+class QuizViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
